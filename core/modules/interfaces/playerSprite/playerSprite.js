7361bd2daf6e7694d0a5c1c95e3133ef59847491
dvfcfbgv <21ianh1@tas.tw>
2019-08-27 02:33:39 +0800

    fix: removed not-used code

--- core/modules/interfaces/playerSprite/playerSprite.js
+++ core/modules/interfaces/playerSprite/playerSprite.js
@@ -59,62 +59,11 @@ class PlayerSprite {
       this.p.image(sprite, wx * (this.isFlipped ? -1 : 1), wy)
     } else this.p.rect(wx, wy, 20, 20)
     this.p.pop()
-
-    // this.flashlight(wx, wy)
   }
 
   flipLeft = () => (this.isFlipped = true)
 
   flipRight = () => (this.isFlipped = false)
-
-  // flashlight = (wx, wy) => {
-  //   // flashlight effect
-  //   // canvas image frame
-  //   this.p.loadPixels()
-
-  //   const lightRadius = 100
-
-  //   for (let y = 0; y < this.p.height; y++) {
-  //     for (let x = 0; x < this.p.width; x++) {
-  //       // // Calculate the 1D location from a 2D grid
-  //       const loc = (x + y * this.p.width) * 4
-  //       // Get the R,G,B values from image
-  //       let r = this.p.pixels[loc]
-  //       let g = this.p.pixels[loc + 1]
-  //       let b = this.p.pixels[loc + 2]
-
-  //       // Calculate an amount to change brightness based on proximity to the mouse
-  //       const distance = this.p.dist(x, y, wx, wy)
-  //       const adjustLightness = this.p.map(distance, 0, lightRadius, 1, 0)
-
-  //       r *= adjustLightness
-  //       g *= adjustLightness
-  //       b *= adjustLightness
-
-  //       r = this.p.constrain(r, 0, 255)
-  //       g = this.p.constrain(r, 0, 255)
-  //       b = this.p.constrain(r, 0, 255)
-
-  //       //   let newLightness = this.p.lightness(rgb)
-  //       //   newLightness += adjustLightness
-  //       //   // Constrain RGB to make sure they are within 0-255 color range
-  //       //   newLightness = this.p.constrain(newLightness, 0, 255)
-  //       //   let hsv = this.p.color(
-  //       //     'hsl(this.p.hue(rgb), this.p.saturation(rgb), newLightness)'
-  //       //   )
-  //       //   rgb = this.p.color(this.p.red(hsv), this.p.green(hsv), this.p.blue(hsv))
-
-  //       // Make a new color and set pixel in the window
-  //       // color c = color(r, g, b);
-  //       this.p.pixels[loc] = r
-  //       this.p.pixels[loc + 1] = g
-  //       this.p.pixels[loc + 2] = b
-  //       this.p.pixels[loc + 3] = 255
-  //     }
-  //   }
-
-  //   this.p.updatePixels()
-  // }
 }
 
 export default PlayerSprite
