dfcc050635d66232cd056130744638a9c5c2b3cf
DanielK1245 <datkoolness@gmail.com>
2019-08-27 01:53:57 +0800

    feat: light

--- core/modules/interfaces/playerSprite/playerSprite.js
+++ core/modules/interfaces/playerSprite/playerSprite.js
@@ -26,6 +26,7 @@ class PlayerSprite {
     /* -------------------------------------------------------------------------- */
     /*                              DRAW PLAYER HERE                              */
     /* -------------------------------------------------------------------------- */
+    this.flashlight(wx, wy)
     this.p.push()
     this.p.rectMode(this.p.CENTER)
     this.p.fill('#eae7af')
@@ -45,6 +46,58 @@ class PlayerSprite {
   flipLeft = () => (this.isFlipped = true)
 
   flipRight = () => (this.isFlipped = false)
+
+  flashlight = (wx, wy) => {
+    //flashlight effect
+    //canvas image frame
+    this.cimg = this.p.get()
+    this.p.pixelDensity(0.5)
+    this.cimg.loadPixels()
+
+    for (let x = 0; x < this.cimg.width; x++) {
+      for (let y = 0; y < this.cimg.height; y++) {
+        //this.cimg.set(x, y, this.p.color(0, 90, 102))
+        // // Calculate the 1D location from a 2D grid
+        let loc = (x + y * this.cimg.width) * 4
+        // Get the R,G,B values from image
+        let r, g, b
+        r = this.cimg.pixels[loc]
+        g = this.cimg.pixels[loc + 1]
+        b = this.cimg.pixels[loc + 2]
+
+        // Calculate an amount to change brightness based on proximity to the mouse
+        let lightRadius = 100
+        var distance = this.p.dist(x, y, wx, wy)
+        var adjustLightness = this.p.map(distance, 0, lightRadius, 1, 0)
+        r *= adjustLightness
+        r = this.p.constrain(r, 0, 255)
+        g *= adjustLightness
+        g = this.p.constrain(r, 0, 255)
+        b *= adjustLightness
+        b = this.p.constrain(r, 0, 255)
+
+        //   let newLightness = this.p.lightness(rgb)
+        //   newLightness += adjustLightness
+        //   // Constrain RGB to make sure they are within 0-255 color range
+        //   newLightness = this.p.constrain(newLightness, 0, 255)
+        //   let hsv = this.p.color(
+        //     'hsl(this.p.hue(rgb), this.p.saturation(rgb), newLightness)'
+        //   )
+        //   rgb = this.p.color(this.p.red(hsv), this.p.green(hsv), this.p.blue(hsv))
+
+        // Make a new color and set pixel in the window
+        //color c = color(r, g, b);
+        let pixloc = (y * this.cimg.width + x) * 4
+        this.cimg.pixels[pixloc] = r
+        this.cimg.pixels[pixloc + 1] = g
+        this.cimg.pixels[pixloc + 2] = b
+        this.cimg.pixels[pixloc + 3] = 255
+      }
+    }
+    this.cimg.updatePixels()
+    this.p.image(this.cimg, 100, 80, 100, 80)
+    this.p.set(0, 0, this.cimg)
+  }
 }
 
 export default PlayerSprite
