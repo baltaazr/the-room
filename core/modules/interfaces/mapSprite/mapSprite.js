4602512dd6be34e592106fb1f10eff65f4926b7b
freshkoala <zunigaruizb@gmail.com>
2019-08-27 09:15:51 +0800

    feat: fix enemy through tunnel

--- core/modules/interfaces/mapSprite/mapSprite.js
+++ core/modules/interfaces/mapSprite/mapSprite.js
@@ -136,11 +136,11 @@ class MapSprite {
           -(ROOM_RADIUS + 1) * FLOOR_SIZE + WINDOW_COORDS.y
         )
         for (let i = -1; i < 2; i++) {
-          if (room.getValTunnels(0, i) === ENEMY_GRID_VAL) {
-            this.p.image(
-              this.floors[room.floorGridTunnels[0][1]],
+          if (room.getValTunnels(0, i + 1) === ENEMY_GRID_VAL) {
+            this.p.circle(
               WINDOW_COORDS.x + FLOOR_SIZE * i,
-              -(ROOM_RADIUS + 1) * FLOOR_SIZE + WINDOW_COORDS.y
+              -(ROOM_RADIUS + 1) * FLOOR_SIZE + WINDOW_COORDS.y,
+              10
             )
           }
         }
@@ -180,11 +180,11 @@ class MapSprite {
           (ROOM_RADIUS + 1) * FLOOR_SIZE + WINDOW_COORDS.y
         )
         for (let i = -1; i < 2; i++) {
-          if (room.getValTunnels(1, i) === ENEMY_GRID_VAL) {
-            this.p.image(
-              this.floors[room.floorGridTunnels[1][1]],
+          if (room.getValTunnels(1, i + 1) === ENEMY_GRID_VAL) {
+            this.p.circle(
               WINDOW_COORDS.x + FLOOR_SIZE * i,
-              (ROOM_RADIUS + 1) * FLOOR_SIZE + WINDOW_COORDS.y
+              (ROOM_RADIUS + 1) * FLOOR_SIZE + WINDOW_COORDS.y,
+              10
             )
           }
         }
@@ -225,11 +225,11 @@ class MapSprite {
           FLOOR_SIZE + WINDOW_COORDS.y + FLOOR_SIZE
         )
         for (let i = -1; i < 2; i++) {
-          if (room.getValTunnels(2, i) === ENEMY_GRID_VAL) {
-            this.p.image(
-              this.floors[room.floorGridTunnels[2][1]],
+          if (room.getValTunnels(2, i + 1) === ENEMY_GRID_VAL) {
+            this.p.circle(
               -(ROOM_RADIUS + 1) * FLOOR_SIZE + WINDOW_COORDS.x,
-              WINDOW_COORDS.y + FLOOR_SIZE * i
+              WINDOW_COORDS.y + FLOOR_SIZE * i,
+              10
             )
           }
         }
@@ -269,11 +269,11 @@ class MapSprite {
           FLOOR_SIZE + WINDOW_COORDS.y + FLOOR_SIZE
         )
         for (let i = -1; i < 2; i++) {
-          if (room.getValTunnels(3, i) === ENEMY_GRID_VAL) {
-            this.p.image(
-              this.floors[room.floorGridTunnels[3][1]],
+          if (room.getValTunnels(3, i + 1) === ENEMY_GRID_VAL) {
+            this.p.circle(
               (ROOM_RADIUS + 1) * FLOOR_SIZE + WINDOW_COORDS.x,
-              WINDOW_COORDS.y + FLOOR_SIZE * i
+              WINDOW_COORDS.y + FLOOR_SIZE * i,
+              10
             )
           }
         }
