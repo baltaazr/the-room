bcc45b9ab8aa8c5bf2a8eadbeabdbe31b23aff15
ian13456 <21ianh1@tas.tw>
2019-08-29 11:40:09 +0800

    Updates

---
+++ precache-manifest.55c63fb1caf5f98af12658cb19cefe59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7789a00bb83b27144ed4b7e894513ce7",
+    "url": "/the-room/index.html"
+  },
+  {
+    "revision": "e6df4419f092ec1b1680",
+    "url": "/the-room/static/css/main.340c2443.chunk.css"
+  },
+  {
+    "revision": "f98af466ada78a597435",
+    "url": "/the-room/static/js/2.7687f5e9.chunk.js"
+  },
+  {
+    "revision": "e6df4419f092ec1b1680",
+    "url": "/the-room/static/js/main.0fbf17cd.chunk.js"
+  },
+  {
+    "revision": "1dab862c71e9a0946b93",
+    "url": "/the-room/static/js/runtime~main.8897cd81.js"
+  },
+  {
+    "revision": "d30c4780c7fc9aee5f27472c94764296",
+    "url": "/the-room/static/media/Dungeon_Tileset.d30c4780.png"
+  },
+  {
+    "revision": "dc65f600fa409e72b217c805f05a93ce",
+    "url": "/the-room/static/media/logo.dc65f600.png"
+  }
+]);
